4219c0e4f245dd191ecca4eec9131e076a69085b
DerLed <48630545+DerLed@users.noreply.github.com>
2022-05-01 17:36:34 +0300

    Пернес реализацию игры в класс Game

--- Week2_Lesson2/src/main/java/Main.java
+++ Week2_Lesson2/src/main/java/Main.java
@@ -1,42 +1,14 @@
-import core.hero.Hero;
-import core.item.Boots;
-import core.item.ChestPlate;
-import core.item.Helmet;
-import core.level.Level;
-import helper.HeroFactory;
+import core.game.Game;
+
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Hero archer = HeroFactory.createArcher();
-        archer.attack(archer);
-        archer.isDeath();
-        if (Math.random() > 0.5)
-            archer.getProtects().add(new Helmet());
-        Hero warrior = HeroFactory.createWarrior();
-        if (Math.random() > 0.5)
-            warrior.getProtects().add(new ChestPlate());
-        Hero mage = HeroFactory.createMage();
-        if (Math.random() > 0.5)
-            mage.getProtects().add(new ChestPlate());
-        Hero elf = HeroFactory.createElf();
-        if (Math.random() > 0.5)
-            elf.getProtects().add(new Boots());
-        Hero dwarf = HeroFactory.createDwarf();
+    public static void main(String[] args)  {
 
-        archer.attack(warrior);
-        warrior.attack(mage);
-        mage.attack(elf);
-        elf.attack(dwarf);
-        dwarf.attack(archer);
+        Scanner scanner = new Scanner(System.in);
+        Game game = new Game(scanner);
+        game.start();
+        scanner.close();
 
-        archer.attack(warrior);
-        archer.updateLevel(Level.L2);
-        warrior.attack(mage);
-        mage.attack(elf);
-        mage.updateLevel(Level.L2);
-        elf.attack(dwarf);
-        elf.updateLevel(Level.L3);
-        dwarf.attack(archer);
-        dwarf.updateLevel(Level.L4);
     }
 }
