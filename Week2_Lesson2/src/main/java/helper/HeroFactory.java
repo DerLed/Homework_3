7bcf3bcf576008705652fc0a036445fc1fb17216
DerLed <48630545+DerLed@users.noreply.github.com>
2022-05-01 12:47:01 +0300

    Вынесены переменные задающие дефолтные значения в класс констант

--- Week2_Lesson2/src/main/java/helper/HeroFactory.java
+++ Week2_Lesson2/src/main/java/helper/HeroFactory.java
@@ -2,28 +2,32 @@ package helper;
 
 import core.hero.*;
 
+import java.util.Properties;
+
 public class HeroFactory {
+
+
     private HeroFactory() {
         throw new UnsupportedOperationException();
     }
 
     public static Hero createArcher() {
-        return new Archer(10);
+        return new Archer(Constant.DEFAULT_ARCHER_DAMAGE);
     }
 
     public static Hero createWarrior() {
-        return new Warrior(30);
+        return new Warrior(Constant.DEFAULT_WARRIOR_DAMAGE);
     }
 
     public static Hero createMage() {
-        return new Mage(5, 10);
+        return new Mage(Constant.DEFAULT_MAGE_DAMAGE, Constant.DEFAULT_MAGE_MANA);
     }
 
     public static Hero createElf() {
-        return new Elf(0);
+        return new Elf(Constant.DEFAULT_ELF_DAMAGE);
     }
 
     public static Hero createDwarf() {
-        return new Dwarf(75);
+        return new Dwarf(Constant.DEFAULT_DWARF_DAMAGE);
     }
 }
