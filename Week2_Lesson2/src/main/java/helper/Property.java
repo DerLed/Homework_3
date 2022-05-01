f78e020e0a0aace5de9b3d9a05ccebe0ea7d7f6e
DerLed <48630545+DerLed@users.noreply.github.com>
2022-05-01 17:37:25 +0300

    Класс для работы с файлом проперти

--- Week2_Lesson2/src/main/java/helper/Property.java
+++ Week2_Lesson2/src/main/java/helper/Property.java
@@ -6,7 +6,10 @@ import java.io.IOException;
 import java.util.Objects;
 import java.util.Properties;
 
-
+/**
+ * Данный класс создает единственный способ обращения к файлу property во всей программе
+ * так как в моей реализации это происходит в нескольких местах
+ */
 public final class Property {
     private static Property property;
     private final Properties properties;
